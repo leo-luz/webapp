9271647787318378ca5bbe52f09c4ca2f8180f1f
leonardocharles <leonardocharles@outlook.com>
2020-03-10 16:53:27 -0300

    Primeiro check-in

---
+++ Models/Endereco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Endereco : Entity
+    {
+        public Guid FornecedorId { get; set; }
+        public string Rua { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string CEP { get; set; }
+
+        /*EF Relação*/
+        public Fornecedor Fornecedor { get; set; }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+        public Guid MarcaId { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public Fornecedor Fornecedor { get; set; }
+        public Marca Marca { get; set; }
+    }
+}
