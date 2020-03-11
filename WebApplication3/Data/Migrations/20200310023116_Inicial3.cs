565c2f48b3acfd45f7edbed8a6c1050f2ac3a2a4
leonardocharles <leonardocharles@outlook.com>
2020-03-10 21:29:48 -0300

    Correção WebAPP

---
+++ WebApplication3/Data/Migrations/20200310023116_Inicial3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication3.Data.Migrations
+{
+    public partial class Inicial3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MarcaId",
+                table: "Produtos",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Marca",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marca", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_MarcaId",
+                table: "Produtos",
+                column: "MarcaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Marca_MarcaId",
+                table: "Produtos",
+                column: "MarcaId",
+                principalTable: "Marca",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Marca_MarcaId",
+                table: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Marca");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Produtos_MarcaId",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "MarcaId",
+                table: "Produtos");
+        }
+    }
+}
---
+++ WebApplication3/Models/Fornecedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Fornecedor : Entity
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "O {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 10)]
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public Endereco Endereco { get; set; }
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
